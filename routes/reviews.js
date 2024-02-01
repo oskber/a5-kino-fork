208b05959bf3cf4cd01b81cb85b644b9e47d99e6
ANschoolacc <145000303+ANschoolacc@users.noreply.github.com>
2024-02-01 20:01:25 +0100

    Removed unnecessary route

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,14 +1,9 @@
 import express from "express";
-import { getReviews, getReviewsSizeFive } from "../utils/reviewsUtils.js";
+import { getReviewsSizeFive } from "../utils/reviewsUtils.js";
 const reviewRouter = express.Router();
 
 reviewRouter.get('/movies/:id/reviews/:page', async (req, res) => {
   const payload = await getReviewsSizeFive(req.params.id, req.params.page)
-  res.json(payload);
-});
-
-reviewRouter.get('/movies/:id/reviews', async (req, res) => {
-  const payload = await getReviews(req.params.id)
   res.json(payload);
 });
 
