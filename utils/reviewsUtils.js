c1bdea79c6c667fd568b4b0c51d15842a1117272
ANschoolacc <145000303+ANschoolacc@users.noreply.github.com>
2024-02-01 20:03:30 +0100

    Removed unnecessary function and added mapped review object

--- utils/reviewsUtils.js
+++ utils/reviewsUtils.js
@@ -3,49 +3,13 @@ import fetch from "node-fetch";
 
 
 //Get reviews for each movie and select page
-
 export async function getReviewsSizeFive(id, page){
   const res = await fetch(`${API_BASE}/reviews?filters[movie]=${id}&pagination[pageSize]=5&pagination[page]=${page}`)
   const payload = await res.json();
-  return payload;
+    return payload.data.map((obj) => ({
+    id: obj.id,
+    ...obj.attributes,
+  }));
 }
-
-export async function getReviews(id){
-  const res = await fetch(`${API_BASE}/reviews?filters[movie]=${id}`)
-  const payload = await res.json();
-  return payload;
-}
-
-
-/*app.get('/api/movies/:id/reviews/:page', async (req, res) =>{
-    const payload = await getReviews(req.params.id, req.params.page)
-    res.send(payload);
-})
-
-
-//Get paginated reviews "in chunks"
-async function getReviewPagination(page){
-    const res = await fetch(`${API_BASE}/reviews?pagination[page]=${page}`)
-    const payload = await res.json()
-    return payload;
-}
-
-app.get('/api/reviews/:page', async (req, res) => {
-    const payload = await getReviewPagination( req.params.page);
-    res.send(payload)
-
-})
-
-async function getReviewPageSize(size){
-    const res = await fetch(`${API_BASE}/reviews?pagination[pageSize]=${size}`)
-    const payload = await res.json()
-    return payload;
-}
-
-/*app.get('/api/reviews/pagesize/:pageSize', async (req, res) => {
-    const payload = await getReviewPageSize( req.params.pageSize); 
-    res.send(payload)
-
-})*/
 
 
