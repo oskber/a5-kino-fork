aead0be5a9f528d0dfe502ee5ba6791bb276d603
ANschoolacc <145000303+ANschoolacc@users.noreply.github.com>
2024-02-02 11:22:18 +0100

    Made changes to frontend script

--- public/renderReviews.js
+++ public/renderReviews.js
@@ -1,35 +1,53 @@
+const currentPath = window.location.pathname;
+const pathId = currentPath.substring(8);
 
-if(window.location.pathname.includes('/movies/')){
-  console.log('Moviepage!')
-  const currentPath = window.location.pathname;
+if (!pathId == '') {
+  document.querySelector('#reviews__prev').style.display = 'none';
   let page = 1;
-  renderReview( await fetchReviews(currentPath, page));
-  document.querySelector('#reviews__btn').addEventListener('click', async () => {
-    page++ 
-    let reviewArr = await fetchReviews(currentPath, page)
-    if( reviewArr.length > 0){
-    const reviewsNode = document.querySelector('#reviews__list')
-    while (reviewsNode.firstElementChild) {
-      reviewsNode.removeChild(reviewsNode.lastElementChild)
-    }
-    renderReview(await fetchReviews(currentPath, page));
-  }
+  renderReview(await fetchReviews(currentPath, page));
 
+  document.querySelector('#reviews__next').addEventListener('click', async () => {
+    document.querySelector('#reviews__prev').style.display = 'block';
+    page++
+    let reviewArr = await fetchReviews(currentPath, page)
+
+    if (reviewArr.length > 0) {
+      const reviewsNode = document.querySelector('#reviews__list')
+      while (reviewsNode.firstElementChild) {
+        reviewsNode.removeChild(reviewsNode.lastElementChild)
+      }
+      renderReview(await fetchReviews(currentPath, page));
+    } else {
+      page--;
+      document.querySelector('#reviews__next').style.display = 'none';
+    }
+  })
+
+  document.querySelector('#reviews__prev').addEventListener('click', async () => {
+    document.querySelector('#reviews__next').style.display = 'block';
+    page--
+      const reviewsNode = document.querySelector('#reviews__list')
+      while (reviewsNode.firstElementChild) {
+        reviewsNode.removeChild(reviewsNode.lastElementChild)
+      }
+      renderReview(await fetchReviews(currentPath, page));
+    if (page == 1){
+      document.querySelector('#reviews__prev').style.display = 'none';
+    }
   })
 }
 
-async function fetchReviews(path, page){
+async function fetchReviews(path, page) {
   const response = await fetch(`/api/${path}/reviews/${page}`);
-  if(response.ok) {
+  if (response.ok) {
     let payload = await response.json();
-      return payload;
+    return payload;
   } else {
-      throw new Error(`Something went wrong with the request. Error code: ${response.status}`)
+    throw new Error(`Something went wrong with the request. Error code: ${response.status}`)
   }
-
 }
 
-async function renderReview(reviewsArr){
+async function renderReview(reviewsArr) {
   console.log(reviewsArr)
   reviewsArr.forEach(obj => {
     const reviewTemplate = document.querySelector('#reviewTemplate');
@@ -44,6 +62,6 @@ async function renderReview(reviewsArr){
     reviewrating.textContent = obj.rating;
     document.querySelector('#reviews__list').appendChild(temp);
   });
-  
+
 }
 
