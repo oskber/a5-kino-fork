4b12d31f375c131859af443f40fc1ef1205c543c
Max Lindström <max.lindstrom2@lerniastudent.se>
2024-02-06 11:26:35 +0100

    Update server log message to include dynamic port

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import app  from './src/app.js'
 const port = process.env.PORT || 5080;
 
 app.listen(port, () => {
-    console.log("Server listening on port 5080");
+    console.log(`Server is running on port ${port}`);
 });
