da005ea3cdfebf9bbb641548e65502f455c2ad5f
ANschoolacc <145000303+ANschoolacc@users.noreply.github.com>
2024-02-06 11:13:46 +0100

    Moved logic to utils

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,11 +4,6 @@ const reviewRouter = express.Router();
 
 reviewRouter.get('/movies/:id/reviews/:page', async (req, res) => {
   const payload = await getReviewsSizeFive(req.params.id, req.params.page)
-  payload.forEach(obj => {
-    if(obj.author == null){
-      obj.author = 'Okänd'
-    }
-  });
   res.json(payload);
 });
 
