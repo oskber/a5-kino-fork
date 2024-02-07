ceea522a7e958bed4db132d6951d4319a4185876
ANschoolacc <145000303+ANschoolacc@users.noreply.github.com>
2024-02-07 13:14:46 +0100

    Changed route logic to include cmsadapter

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { getReviewsSizeFive } from "../utils/reviewsUtils.js";
+import cmsAdapter from "../src/cmsAdapt.js";
 const reviewRouter = express.Router();
 
 reviewRouter.get('/movies/:id/reviews/:page', async (req, res) => {
-  const payload = await getReviewsSizeFive(req.params.id, req.params.page)
+  const payload = await getReviewsSizeFive(cmsAdapter ,req.params.id, req.params.page)
   res.json(payload);
 });
 
