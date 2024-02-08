1e2337ccd871452e0d829f9613d590f1c5def9c6
ANschoolacc <145000303+ANschoolacc@users.noreply.github.com>
2024-02-08 16:16:44 +0100

    Changed test
    Divided my one test into three, removed imported mocked adapter, made function for mocking reviews, changed tests to have an adapter inside them and using mocked review function inside adapter

--- tests/reviews.test.js
+++ tests/reviews.test.js
@@ -1,20 +1,78 @@
 import { describe, test, expect } from "@jest/globals";
 import { getReviewsSizeFive } from "../utils/reviewsUtils";
-import { mockedAdapt } from "./__mocks__/mockedAdapt";
 
-describe('getReviewsSizeFive',() => {
-  test('Expect an array of max five verified reviews, and expect reviews to have a rating between 0-5', async () =>{
+describe('getReviewsSizeFive', () => {
+  test('Expect an array of max five reviews', async () => {
 
-    const data = await getReviewsSizeFive(mockedAdapt, 1, 1);
+    const mockedAdapter = {
+      loadMoviesReviews: async () => ([
+        mockedReview(),
+        mockedReview(),
+        mockedReview(),
+        mockedReview(),
+        mockedReview(),
+        mockedReview()
+      ])
+    }
+
+    const data = await getReviewsSizeFive(mockedAdapter, 'id of movie', 1);
 
     expect(data).toHaveLength(5);
 
+  });
+
+  test('Only return movie reviews with rating between 0 and 5', async () => {
+
+    const mockedAdapter = {
+      loadMoviesReviews: async () => ([
+        mockedReview({ rating: 3 }),
+        mockedReview({ rating: 6 }),
+        mockedReview({ rating: -1 }),
+        mockedReview({ rating: null })
+      ])
+    }
+
+    const data = await getReviewsSizeFive(mockedAdapter, 'id of movie', 1);
+
     data.forEach((obj) => {
-      expect(obj.verified).toBe(true);
       expect(obj.rating).toBeGreaterThanOrEqual(0);
       expect(obj.rating).toBeLessThanOrEqual(5);
-
     })
-  });
-}); 
+    expect(data).toHaveLength(1)
 
+  });
+
+  test('Only return movie reviews that are verified(verified: true)', async () => {
+
+    const mockedAdapter = {
+      loadMoviesReviews: async () => ([
+        mockedReview({ verified: false }),
+        mockedReview({ verified: null }),
+        mockedReview({ verified: true })
+      ])
+    }
+
+    const data = await getReviewsSizeFive(mockedAdapter, 'id of movie', 1);
+
+    data.forEach((obj) => {
+      expect(obj.verified).toBe(true);
+    });
+    expect(data).toHaveLength(1);
+
+  })
+});
+
+function mockedReview(attributes) {
+  return {
+    id: 307,
+    attributes: {
+      comment: "test",
+      rating: 5,
+      author: "test",
+      verified: true,
+      createdAt: "2024-02-02T14:20:22.972Z",
+      updatedAt: "2024-02-02T14:20:22.972Z",
+      ...attributes
+    }
+  }
+}
