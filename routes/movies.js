9e7b594fd6acf390a9cf841f2101d2e45bf18f7b
PedalConny <pedalconny@hotmail.com>
2024-02-08 14:38:23 +0100

    added frontend to ratingBackend to get rating visable

--- routes/movies.js
+++ routes/movies.js
@@ -1,27 +1,46 @@
-import express from 'express';
-import { getMovieById, getMovies } from '../utils/movieUtils.js';
+import express from "express";
+import { getMovieById, getMovies } from "../utils/movieUtils.js";
+import { getAverageRating } from "../utils/reviewsUtils.js";
 
 const movieRouter = express.Router();
 
-movieRouter.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    const movie = await getMovieById(id);
-    if (!movie.data) {
-        res.status(404);
-        res.render('error', { message: `No movie with ID ${id} found` })
-        return
-    }
-    res.render('movie', { movie: movie.data });
+movieRouter.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const movie = await getMovieById(id);
+
+  const result = await getAverageRating(id, movie.data.attributes.imdbId);
+  if (!movie.data) {
+    res.status(404);
+    res.render("error", { message: `No movie with ID ${id} found` });
+    return;
+  }
+
+  movie.data.rating = result.rating;
+  movie.data.reviewsCount = result.reviewsCount;
+
+  res.render("movie", { movie: movie.data });
 });
 
-movieRouter.get('/', async (req, res) => {
-    const movies = await getMovies();
-    if (!movies.data) {
-        res.status(404);
-        res.render('error', { message: 'No movies found' })
-        return
-    }
-    res.render('movies', { movies: movies.data });
+movieRouter.get("/", async (req, res) => {
+  const movies = await getMovies();
+  if (!movies.data) {
+    res.status(404);
+    res.render("error", { message: "No movies found" });
+    return;
+  }
+
+  const moviesWithRatings = await Promise.all(
+    movies.data.map(async (movie) => {
+      console.log(movie);
+      const result = await getAverageRating(movie.id, movie.attributes.imdbId);
+      return {
+        ...movie,
+        rating: result.rating,
+        reviewsCount: result.reviewsCount,
+      };
+    })
+  );
+  res.render("movies", { movies: moviesWithRatings });
 });
 
 export default movieRouter;
