697306fca7348e26ed16ea65e189de04d0704173
Max Lindström <max.lindstrom2@lerniastudent.se>
2024-02-10 11:37:52 +0100

    Removed unused function

--- utils/omdbApiUtils.js
+++ utils/omdbApiUtils.js
@@ -11,12 +11,3 @@ export const imdbAdapter = {
         return omdbData.imdbRating;
     },
 };
-
-export async function imdbRating(id) {
-    const res = await fetch(`${API_BASE}/movies/${id}`);
-    const payload = await res.json();
-    const imdbId = payload.data.attributes.imdbId;
-    const omdbApi = await fetch(`http://www.omdbapi.com/?i=${imdbId}&apikey=5c14f842`);
-    const omdbData = await omdbApi.json();
-    return omdbData.imdbRating;
-}
